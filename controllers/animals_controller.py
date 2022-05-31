9e503aa8c536ea876ae8a2811d2a006ecfebb81e
Jennifer Ramsay <Jenny.Ramsay@gmail.com>
2022-05-31 13:16:35 +0100

    fixed the add new pet

--- controllers/animals_controller.py
+++ controllers/animals_controller.py
@@ -3,7 +3,6 @@ from flask import Blueprint
 from repositories import animal_repository
 from repositories import vet_repository
 from models.animal import Animal
-from models.vet import Vet
 
 
 animals_blueprint = Blueprint("animals", __name__)
@@ -17,8 +16,26 @@ def animals():
 
 @animals_blueprint.route("/register/animal", methods= ['Get'])
 def register_animals():
-    animal = animal_repository.select_all()
-    return render_template("animals/register_animal.html", all_vets = animal)
+    vets = vet_repository.select_all()
+    return render_template("animals/register_animal.html", all_vets = vets)
+
+# CREATE
+# POST '/animals'
+
+@animals_blueprint.route("/animals",  methods=['POST'])
+def create_pet():
+    animal_name = request.form['animal_name']
+    vet_id     = request.form['vet_id']
+    date_of_birth = request.form['date_of_birth']
+    animal_type   = request.form['animal_type']
+    owner_contact = request.form['owner_contact']
+    treatment_notes = request.form['treatment_notes']
+
+    vet        = vet_repository.select(vet_id)
+    animal        = Animal(animal_name, vet,date_of_birth, animal_type, owner_contact, treatment_notes)
+
+    animal_repository.save(animal)
+    return redirect('/animals')
 
 
 @animals_blueprint.route("/animal/<id>/info", methods=['GET'])
